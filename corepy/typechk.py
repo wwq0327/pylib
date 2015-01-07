f5f033ed66cae4a945a551170bd91544a6045d86
wwq0327 <wwq0327@gmail.com>
2015-01-07 21:02:17 +0800

    f

---
+++ corepy/typechk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def displayNumType(num):
+    print num, 'is', 
+    if isinstance(num, (int, long, float, complex)):
+        print 'a number of type: ', type(num).__name__
+    else:
+        print 'not a number at all!!'
+
+displayNumType(-69)
+displayNumType(99999999999999L)
+displayNumType(-5.2+1.9j)
+displayNumType('xxxx')
